a4baa751b4047d354e4cd63cf336aa3440aee9a1
enoshbrainard <158431698+enoshbrainard@users.noreply.github.com>
2025-06-22 17:14:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://chatai-3qu5.vercel.app
+", credentials: true }));
 app.use(express.json());
 
 // Routes
